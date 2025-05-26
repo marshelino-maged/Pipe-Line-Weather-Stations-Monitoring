c171e5b308f39f8d33f456bef2e74336217b24dd
Ehab Yasser <97472134+EhabYasser25@users.noreply.github.com>
2025-05-26 04:23:54 +0300

    kibana configured

--- bitcask/src/bitcask/App.java
+++ bitcask/src/bitcask/App.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BitCaskEngine engine = new BitCaskEngine("data/segments", 1024 * 1024, true);
+        BitCaskEngine engine = new BitCaskEngine("D:\\Study\\Semester 8\\Data Intensive\\Project\\Pipe-Line-Weather-Stations-Monitoring\\data\\bitcask", 1024 * 1024, true);
 
         if (args.length == 0 || args[0].equals("--help")) {
             System.out.println("Usage:");
